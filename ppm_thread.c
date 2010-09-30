f58f95dcf060f9d3edb48de75d411a684666df19
Omar Awile <omar@nausicaa.inf.ethz.ch>
2010-09-30 22:30:46 +0200

    - added mutex routines

--- ppm_thread.c
+++ ppm_thread.c
@@ -40,6 +40,9 @@ int is_initialized = 0;
 pthread_mutex_t thread_create_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t thread_join_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t thread_attr_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_attr_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_init_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_destroy_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
 /**
@@ -55,6 +58,20 @@ array_t *threads = NULL;
  * threads at the same time.
  **/
 array_t *thread_attrs = NULL;
+
+/*
+ * holds the mutex IDs
+ **/
+array_t *mutex = NULL;
+
+/**
+ * holds thread mutex attributes, the index does not
+ * necesseraly concide with the one of mutexes.
+ *
+ * This array is just to allow different threads handle
+ * their mutexes
+ **/
+array_t *mutex_attrs = NULL;
 
 /**
  * an array to hold pthread_once_t structures
@@ -99,6 +116,8 @@ int ppm_cthread_init() {
     array_init(&threads,INIT_SIZE);
     array_init(&thread_attrs,INIT_SIZE);
     array_init(&once_ctrls,INIT_SIZE);
+    array_init(&mutex,INIT_SIZE);
+    array_init(&mutex_attrs,INIT_SIZE);
     
     // allocate and store the thread master ID
     threads->data[0] = (pthread_t*) malloc(sizeof(pthread_t));
@@ -123,10 +142,15 @@ int is_valid(array_t *arr, int id) {
         return 0;
 }
 
-int ppm_cthread_create(int *thread_id, int *attrs_id, void *(*start_routine)(void *),void *arg) {
+/*****************************************/
+/*         Thread routines               */
+/*****************************************/
+
+
+int ppm_cthread_create(int *thread_id, int *attr_id, void *(*start_routine)(void *),void *arg) {
     int i = 0;
     int info = 0;
-    pthread_attr_t *attrs;
+    pthread_attr_t *attr;
     
     if (!is_initialized)
         return PPM_EINIT;
@@ -138,15 +162,15 @@ int ppm_cthread_create(int *thread_id, int *attrs_id, void *(*start_routine)(voi
     }
     threads->data[threads->after] = (pthread_t*) malloc(sizeof(pthread_t));
     
-    if (*attrs_id == -1) {
-        attrs = (pthread_attr_t*) malloc(sizeof(pthread_attr_t));
-        pthread_attr_init(attrs);
-        pthread_attr_setdetachstate(attrs, PTHREAD_CREATE_JOINABLE);
+    if (*attr_id == -1) {
+        attr = (pthread_attr_t*) malloc(sizeof(pthread_attr_t));
+        pthread_attr_init(attr);
+        pthread_attr_setdetachstate(attr, PTHREAD_CREATE_JOINABLE);
     } else {
-        attrs = thread_attrs->data[*attrs_id];
+        attr = thread_attrs->data[*attr_id];
     }
 
-    info = pthread_create(threads->data[threads->after], attrs, start_routine, arg);
+    info = pthread_create(threads->data[threads->after], attr, start_routine, arg);
 
     if (info) {
         pthread_mutex_unlock(&thread_create_mutex);
@@ -156,8 +180,8 @@ int ppm_cthread_create(int *thread_id, int *attrs_id, void *(*start_routine)(voi
     *thread_id = threads->after;
     threads->after++;
 
-    if (*attrs_id == -1)
-        free(attrs);
+    if (*attr_id == -1)
+        free(attr);
 
     pthread_mutex_unlock(&thread_create_mutex);
     
@@ -271,6 +295,10 @@ int ppm_cthread_atfork(void (*prepare)(void), void (*parent)(void), void (*child
     return pthread_atfork(prepare,parent,child);
 }
 
+/*****************************************/
+/*      attribute object routines        */
+/*****************************************/
+
 int ppm_cthread_attr_destroy(int *attr) {
     int info = 0;
 
@@ -290,7 +318,229 @@ int ppm_cthread_attr_destroy(int *attr) {
         pthread_mutex_unlock(&thread_attr_mutex);
         return info;
     }
+    free(thread_attrs->data[*attr]);
     thread_attrs->data[*attr] = NULL;
     
     pthread_mutex_unlock(&thread_attr_mutex);
+    
+    return 0;
 }
+
+
+int ppm_cthread_attr_init(int *attr) {
+    int info = 0;
+
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    pthread_mutex_lock(&thread_attr_mutex);
+
+    if (thread_attrs->after == thread_attrs->size) {
+        // we exhausted the thread id and attribute arrays, double space
+        array_resize(&thread_attrs,thread_attrs->size*2);
+    }
+    thread_attrs->data[thread_attrs->after] = 
+        (pthread_attr_t*) malloc(sizeof(pthread_attr_t));
+
+    info = pthread_attr_init(thread_attrs->data[thread_attrs->after]);
+    
+    if (info) {
+        pthread_mutex_unlock(&thread_attr_mutex);
+        return info;
+    }
+
+    *attr = thread_attrs->after;
+    thread_attrs->after++;
+
+    pthread_mutex_unlock(&thread_attr_mutex);
+    
+    return info;
+}
+
+
+/*****************************************/
+/*             mutex routines            */
+/*****************************************/
+
+int ppm_cthread_mutex_destroy(int *mutex_id) {
+    
+    int info = 0;
+
+    if (!is_initialized)
+        return PPM_EINIT;
+    
+    pthread_mutex_lock(&mutex_destroy_mutex);
+    
+    if (!is_valid(mutex,*mutex_id)) {
+        pthread_mutex_unlock(&mutex_destroy_mutex);
+        return PPM_EINVALID;
+    }
+
+    info = pthread_mutex_destroy(((pthread_mutex_t*)(mutex->data[*mutex_id])));
+    
+    if (info) {
+        pthread_mutex_unlock(&mutex_destroy_mutex);
+        return info;
+    }
+
+    free(mutex->data[*mutex_id]);
+    mutex->data[*mutex_id] = NULL;
+    
+    pthread_mutex_unlock(&mutex_destroy_mutex);
+    
+    return 0;
+
+}
+
+
+ppm_cthread_mutex_init(int *mutex_id, int *attr_id) {
+    int i = 0;
+    int info = 0;
+    pthread_mutexattr_t *attr;
+    
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    pthread_mutex_lock(&mutex_init_mutex);
+    if (mutex->after == mutex->size) {
+        // we exhausted the mutex id array, double space
+        array_resize(&mutex,mutex->size*2);
+    }
+    mutex->data[mutex->after] = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+    
+    if (*attr_id == -1) {
+        attr = NULL;
+    } else {
+        attr = thread_attrs->data[*attr_id];
+    }
+
+    info = pthread_mutex_init((pthread_mutex_t*)(mutex->data[mutex->after]), attr);
+
+    if (info) {
+        pthread_mutex_unlock(&mutex_init_mutex);
+        return info;
+    }
+    
+    *mutex_id = mutex->after;
+    mutex->after++;
+
+    pthread_mutex_unlock(&mutex_init_mutex);
+    
+    return 0;
+
+}
+
+int ppm_cthread_lock(int *mutex_id) {
+    int info = 0;
+    
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    if (!is_valid(mutex,*mutex_id)) {
+        return PPM_EINVALID;
+    }
+
+    return pthread_mutex_lock((pthread_mutex_t*)(mutex->data[*mutex_id]));
+    
+}
+
+int ppm_cthread_trylock(int *mutex_id) {
+    int info = 0;
+
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    if (!is_valid(mutex,*mutex_id)) {
+        return PPM_EINVALID;
+    }
+
+    return pthread_mutex_trylock((pthread_mutex_t*)(mutex->data[*mutex_id]));
+
+}
+
+int ppm_cthread_unlock(int *mutex_id) {
+    int info = 0;
+
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    if (!is_valid(mutex,*mutex_id))
+        return PPM_EINVALID;
+
+    return pthread_mutex_unlock((pthread_mutex_t*)(mutex->data[*mutex_id]));
+
+}
+
+
+
+/*****************************************/
+/*       mutex attribute routines        */
+/*****************************************/
+
+int ppm_cthread_mutexattr_destroy(int *attr) {
+    int info = 0;
+
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    pthread_mutex_lock(&mutex_attr_mutex);
+    
+    if (!is_valid(mutex_attrs,*attr)) {
+        pthread_mutex_unlock(&mutex_attr_mutex);
+        return PPM_EINVALID;
+    }
+    
+    info = pthread_mutexattr_destroy(((pthread_mutexattr_t*)(mutex_attrs->data[*attr])));
+    
+    if (info) {
+        pthread_mutex_unlock(&mutex_attr_mutex);
+        return info;
+    }
+
+    free(mutex_attrs->data[*attr]);
+    mutex_attrs->data[*attr] = NULL;
+    
+    pthread_mutex_unlock(&mutex_attr_mutex);
+    
+    return 0;
+
+
+}
+
+
+int ppm_cthread_mutexattr_init(int *attr) {
+    int info = 0;
+
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    pthread_mutex_lock(&mutex_attr_mutex);
+
+    if (mutex_attrs->after == mutex_attrs->size) {
+        // we exhausted the mutex attribute array, double space
+        array_resize(&mutex_attrs,mutex_attrs->size*2);
+    }
+    mutex_attrs->data[mutex_attrs->after] = 
+        (pthread_mutexattr_t*) malloc(sizeof(pthread_mutexattr_t));
+
+    info = pthread_mutexattr_init(mutex_attrs->data[mutex_attrs->after]);
+    
+    if (info) {
+        pthread_mutex_unlock(&mutex_attr_mutex);
+        return info;
+    }
+
+    *attr = mutex_attrs->after;
+    mutex_attrs->after++;
+
+    pthread_mutex_unlock(&mutex_attr_mutex);
+    
+    return info;
+
+}
+
+
+
+
+
+
