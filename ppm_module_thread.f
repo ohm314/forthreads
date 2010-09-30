8ffa7de37e066cf54fd3886a7185e29cae5baf03
Omar Awile <omar@nausicaa.inf.ethz.ch>
2010-09-30 15:04:22 +0200

    - initial commit

---
+++ ppm_module_thread.f
@@ -0,0 +1,12 @@
+
+MODULE ppm_module_thread
+      !!! This module is a Fortran 90 wrapper to the
+      !!! POSIX pthreads library
+
+
+      CONTAINS
+
+      SUBROUTINE ppm_thread_create(thread_id
+
+
+END ppm_module_thread
---
+++ ppm_thread.c
@@ -0,0 +1,275 @@
+#include <pthread.h>
+#include <stdlib.h>
+
+/*
+ * C wrappers to pthreads to make wrapping with Fortran simpler
+ */
+
+
+/**
+ * global parameters
+ **/
+
+#define INIT_SIZE 4
+
+/**
+ * error codes
+ **/
+#define PPM_EINIT -1
+#define PPM_EINVALID -2
+
+/**
+ * A convenient array type
+ **/
+
+
+typedef struct {
+    void **data;
+    int size;
+    int after;
+} array_t;
+
+/**
+ * global data structures
+ **/
+
+
+int is_initialized = 0;
+
+
+pthread_mutex_t thread_create_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t thread_join_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+/**
+ * holds the thread IDs
+ **/
+array_t *threads;
+
+/** 
+ * holds thread attributes, the index does not
+ * necesseraly conincide with the one of threads.
+ *
+ * This array is just to allow different threads spawn new
+ * threads at the same time.
+ **/
+array_t *thread_attrs;
+
+/**
+ * an array to hold pthread_once_t structures
+ **/
+array_t *once_ctrls;
+
+void array_init(array_t *array,int size) {
+    int i;
+
+    if (array == NULL)
+        array = (array_t*)malloc(sizeof(array_t));
+
+    array->data = (void**)malloc(sizeof(void*)*size);
+    for(i = 0; i < size; i++)
+        array->data[i] = NULL;
+    array->size = size;
+    array->after = 0;
+}
+
+void array_resize(array_t *array,int size) {
+    int i;
+
+    array->data = (void**)realloc(array->data,sizeof(void*)*size);
+    array->size = size;
+    
+    for(i = array->after; i < size; i++)
+        array->data[i] = NULL;
+}
+
+void array_delete(array_t *array) {
+    free(array->data);
+    free(array);
+}
+
+
+int ppm_cthread_init() {
+    int i = 0;
+    int info = 0;
+
+    if (is_initialized)
+        return 0;
+
+    array_init(threads,INIT_SIZE);
+    array_init(thread_attrs,INIT_SIZE);
+    array_init(once_ctrls,INIT_SIZE);
+
+    // allocate and store the thread master ID
+    threads->data[0] = (pthread_t*) malloc(sizeof(pthread_t));
+    threads->data[0] = pthread_self();
+    threads->after++;
+    
+    is_initialized = 1;
+
+    return 0;
+}
+
+int ppm_cthread_destroy() {
+    // TODO: deallocate all, destroy all threads, mutexes
+}
+
+// This only works for pointer arrays!!
+int is_valid(array_t *arr, int id) {
+    if ((id >= 0) && (id < arr->after) && 
+        (arr->data[id] != NULL))
+        return 1;
+    else
+        return 0;
+}
+
+int ppm_cthread_create(int *thread_id, int *attrs_id, void *(*start_routine)(void *),void *arg) {
+    int i = 0;
+    int info = 0;
+    pthread_attr_t *attrs;
+    
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    pthread_mutex_lock(&thread_create_mutex);
+    if (threads->after == threads->size) {
+        // we exhausted the thread id and attribute arrays, double space
+        array_resize(threads,threads->size*2);
+    }
+
+    threads->data[threads->after] = (pthread_t*) malloc(sizeof(pthread_t));
+    
+    if (*attrs_id == -1) {
+        attrs = (pthread_attr_t*) malloc(sizeof(pthread_attr_t));
+        pthread_attr_init(attrs);
+        pthread_attr_setdetachstate(attrs, PTHREAD_CREATE_JOINABLE);
+    } else {
+        attrs = thread_attrs->data[*attrs_id];
+    }
+
+    info = pthread_create(threads->data[threads->after], &attrs, start_routine, arg);
+
+    if (info) {
+        pthread_mutex_unlock(&thread_create_mutex);
+        return info;
+    }
+    
+    *thread_id = threads->after;
+    threads->after++;
+
+    if (*attrs_id == -1)
+        free(attrs);
+
+    pthread_mutex_unlock(&thread_create_mutex);
+    
+    return 0;
+}
+
+int ppm_cthread_detach(int *thread_id) {
+
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    if (!is_valid(threads,*thread_id)) {
+        return PPM_EINVALID;
+    }
+
+    return pthread_detach(threads->data[*thread_id]);
+}
+
+int ppm_cthread_equal(int *t1, int *t2) {
+
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    if (!is_valid(threads,*t1)) {
+        return PPM_EINVALID;
+    }
+    
+    if (!is_valid(threads,*t2)) {
+        return PPM_EINVALID;
+    }
+    
+    return pthread_equal((pthread_t) *((pthread_t*)threads->data[*t1]),
+                (pthread_t) *((pthread_t*)threads->data[*t2]));
+}
+
+int ppm_cthread_exit(void *value_ptr) {
+
+    pthread_exit(value_ptr);
+
+}
+
+int ppm_cthread_join(int *thread_id, void **value_ptr) {
+    int info = 0;
+
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    pthread_mutex_lock(&thread_join_mutex);
+    if (!is_valid(threads,*thread_id)) {
+        return PPM_EINVALID;
+    }
+
+    info = pthread_join(threads->data[*thread_id],value_ptr);
+
+    if (info) {
+        pthread_mutex_unlock(&thread_join_mutex);
+        return info;
+    }
+    
+    free(threads->data[*thread_id]);
+    threads->data[*thread_id] = NULL;
+    
+    pthread_mutex_unlock(&thread_join_mutex);
+    return info;
+}
+
+int ppm_cthread_cancel(int *thread_id) {
+    int info = 0;
+
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    if (!is_valid(threads,*thread_id)) {
+        return PPM_EINVALID;
+    }
+
+    return pthread_cancel(threads->data[*thread_id]);
+
+}
+
+int ppm_cthread_once(int *once_ctrl_id, void (*routine)(void)) {
+    int info = 0;
+
+    if (!is_initialized)
+        return PPM_EINIT;
+
+    if (!is_valid(once_ctrls,*once_ctrl_id)) {
+        return PPM_EINVALID;
+    }
+
+    return pthread_once(once_ctrls->data[*once_ctrl_id],routine);
+
+}
+
+int ppm_cthread_self() {
+    pthread_t tid;
+    int i = 0;
+
+    tid = pthread_self();
+    for (i = 0; i < threads->after; i++) {
+        if (threads->data[i] == NULL)
+            continue;
+        if (pthread_equal(tid,(pthread_t) *((pthread_t*)threads->data[i])))
+            return i;
+    }
+    return -1;
+}
+
+int ppm_cthread_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void)) {
+
+    return pthread_atfork(prepare,parent,child);
+}
+
+
