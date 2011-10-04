0077d0e102f25fc99e23f3a06bea2225204ad5ad
Omar Awile <omar.awile@inf.ethz.ch>
2011-10-04 17:56:42 +0200

    added many attribute accessor wrapper-routines

--- ft_attr.c
+++ ft_attr.c
@@ -69,6 +69,307 @@ void forthread_attr_init(int *attr, int *info) {
 
 }
 
+void forthread_attr_getdetachstate(int *attr, int *detachstate, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_getdetachstate((pthread_attr_t*)(thread_attrs->data[*attr]),detachstate);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+void forthread_attr_setdetachstate(int *attr, int *detachstate, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_setdetachstate((pthread_attr_t*)(thread_attrs->data[*attr]),*detachstate);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+void forthread_attr_getguardsize(int *attr, int *guardsize, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_getguardsize((pthread_attr_t*)(thread_attrs->data[*attr]),(size_t*)guardsize);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+void forthread_attr_setguardsize(int *attr, int *guardsize, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_setguardsize((pthread_attr_t*)(thread_attrs->data[*attr]),(size_t)*guardsize);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+void forthread_attr_getinheritsched(int *attr, int *inheritsched, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_getinheritsched((pthread_attr_t*)(thread_attrs->data[*attr]),inheritsched);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+void forthread_attr_setinheritsched(int *attr, int *inheritsched, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_setinheritsched((pthread_attr_t*)(thread_attrs->data[*attr]),*inheritsched);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+// implements pthreads getschedparam
+void forthread_attr_getschedpriority(int *attr, int *sched_priority, int *info) {
+  *info = FT_OK;
+  struct sched_param param;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_getschedparam((pthread_attr_t*)(thread_attrs->data[*attr]),&param);
+
+  *sched_priority = param.sched_priority;
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+// implements pthreads setschedparam
+void forthread_attr_setschedpriority(int *attr, int *sched_priority, int *info) {
+  *info = FT_OK;
+  struct sched_param param;
+  param.sched_priority = *sched_priority;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_setschedparam((pthread_attr_t*)(thread_attrs->data[*attr]),&param);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+void forthread_attr_getschedpolicy(int *attr, int *policy, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_getschedpolicy((pthread_attr_t*)(thread_attrs->data[*attr]),policy);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+void forthread_attr_setschedpolicy(int *attr, int *policy, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_setschedpolicy((pthread_attr_t*)(thread_attrs->data[*attr]),*policy);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+void forthread_attr_getscope(int *attr, int *scope, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_getscope((pthread_attr_t*)(thread_attrs->data[*attr]),scope);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+void forthread_attr_setscope(int *attr, int *scope, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_setscope((pthread_attr_t*)(thread_attrs->data[*attr]),*scope);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+void forthread_attr_getstacksize(int *attr, int *stacksize, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_getstacksize((pthread_attr_t*)(thread_attrs->data[*attr]),(size_t*)stacksize);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
+void forthread_attr_setstacksize(int *attr, int *stacksize, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(thread_attrs->mutex));
+  if (!is_valid(thread_attrs,*attr)) {
+    pthread_mutex_unlock(&(thread_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_attr_setstacksize((pthread_attr_t*)(thread_attrs->data[*attr]),(size_t)*stacksize);
+
+  pthread_mutex_unlock(&(thread_attrs->mutex));
+
+}
+
 
 /*****************************************/
 /*       mutex attribute routines        */
@@ -134,6 +435,190 @@ void forthread_mutexattr_init(int *attr,int *info) {
 
   *attr = mutex_attrs->after;
   mutex_attrs->after++;
+
+  pthread_mutex_unlock(&(mutex_attrs->mutex));
+
+}
+
+void forthread_mutexattr_getpshared(int *attr, int *pshared, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(mutex_attrs->mutex));
+  if (!is_valid(mutex_attrs,*attr)) {
+    pthread_mutex_unlock(&(mutex_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_mutexattr_getpshared(
+                 (pthread_mutexattr_t*)(mutex_attrs->data[*attr]),
+                 pshared);
+
+  pthread_mutex_unlock(&(mutex_attrs->mutex));
+
+}
+
+void forthread_mutexattr_setpshared(int *attr, int *pshared, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(mutex_attrs->mutex));
+  if (!is_valid(mutex_attrs,*attr)) {
+    pthread_mutex_unlock(&(mutex_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_mutexattr_setpshared(
+                 (pthread_mutexattr_t*)(mutex_attrs->data[*attr]),
+                 *pshared);
+
+  pthread_mutex_unlock(&(mutex_attrs->mutex));
+
+}
+
+void forthread_mutexattr_getprioceiling(int *attr, int *prioceiling, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(mutex_attrs->mutex));
+  if (!is_valid(mutex_attrs,*attr)) {
+    pthread_mutex_unlock(&(mutex_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_mutexattr_getprioceiling(
+                 (pthread_mutexattr_t*)(mutex_attrs->data[*attr]),
+                 prioceiling);
+
+  pthread_mutex_unlock(&(mutex_attrs->mutex));
+
+}
+
+void forthread_mutexattr_setprioceiling(int *attr, int *prioceiling, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(mutex_attrs->mutex));
+  if (!is_valid(mutex_attrs,*attr)) {
+    pthread_mutex_unlock(&(mutex_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_mutexattr_setprioceiling(
+                 (pthread_mutexattr_t*)(mutex_attrs->data[*attr]),
+                 *prioceiling);
+
+  pthread_mutex_unlock(&(mutex_attrs->mutex));
+
+}
+
+void forthread_mutexattr_getprotocol(int *attr, int *protocol, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(mutex_attrs->mutex));
+  if (!is_valid(mutex_attrs,*attr)) {
+    pthread_mutex_unlock(&(mutex_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_mutexattr_getprotocol(
+                 (pthread_mutexattr_t*)(mutex_attrs->data[*attr]),
+                 protocol);
+
+  pthread_mutex_unlock(&(mutex_attrs->mutex));
+
+}
+
+void forthread_mutexattr_setprotocol(int *attr, int *protocol, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(mutex_attrs->mutex));
+  if (!is_valid(mutex_attrs,*attr)) {
+    pthread_mutex_unlock(&(mutex_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_mutexattr_setprotocol(
+                 (pthread_mutexattr_t*)(mutex_attrs->data[*attr]),
+                 *protocol);
+
+  pthread_mutex_unlock(&(mutex_attrs->mutex));
+
+}
+
+void forthread_mutexattr_gettype(int *attr, int *type, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(mutex_attrs->mutex));
+  if (!is_valid(mutex_attrs,*attr)) {
+    pthread_mutex_unlock(&(mutex_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_mutexattr_gettype(
+                 (pthread_mutexattr_t*)(mutex_attrs->data[*attr]),
+                 type);
+
+  pthread_mutex_unlock(&(mutex_attrs->mutex));
+
+}
+
+void forthread_mutexattr_settype(int *attr, int *type, int *info) {
+  *info = FT_OK;
+
+  if (!is_initialized) {
+    *info = FT_EINIT;
+    return;
+  }
+
+  pthread_mutex_lock(&(mutex_attrs->mutex));
+  if (!is_valid(mutex_attrs,*attr)) {
+    pthread_mutex_unlock(&(mutex_attrs->mutex));
+    *info = FT_EINVALID;
+    return;
+  }
+
+  *info = pthread_mutexattr_settype(
+                 (pthread_mutexattr_t*)(mutex_attrs->data[*attr]),
+                 *type);
 
   pthread_mutex_unlock(&(mutex_attrs->mutex));
 
--- ft_attr.h
+++ ft_attr.h
@@ -1,6 +1,23 @@
 #ifndef _FT_ATTR_H_
 #define _FT_ATTR_H_
 
+/**
+ * NOTE:
+ * The routines:
+ * - pthread_attr_setstack 
+ * - pthread_attr_setstackaddr
+ * - pthread_attr_getstack 
+ * - pthread_attr_getstackaddr
+ * are not wrapped and shall not be visible to 
+ * Fortran programs. This had to be done because Fortran generally does not
+ * expose memory addresses to the programmer. For most applications it should
+ * anyway not be necessary for the programmer to allocate the thread stack
+ * manually.
+ *
+ * To explicitly set the size of the stack the routine forthread_setstacksize
+ * should be used.
+ **/
+
 #include <pthread.h>
 #include "ft_consts.h"
 #include "ft_data.h"
@@ -14,6 +31,36 @@ void forthread_attr_destroy(int *attr, int *info);
 
 void forthread_attr_init(int *attr, int *info);
 
+void forthread_attr_getdetachstate(int *attr, int *detachstate, int *info);
+
+void forthread_attr_setdetachstate(int *attr, int *detachstate, int *info);
+
+void forthread_attr_getguardsize(int *attr, int *guardsize, int *info);
+
+void forthread_attr_setguardsize(int *attr, int *guardsize, int *info);
+
+void forthread_attr_getinheritsched(int *attr, int *inheritsched, int *info);
+
+void forthread_attr_setinheritsched(int *attr, int *inheritsched, int *info);
+
+// implements pthreads getschedparam
+void forthread_attr_getschedpriority(int *attr, int *sched_priority, int *info);
+
+// implements pthreads setschedparam
+void forthread_attr_setschedpriority(int *attr, int *sched_priority, int *info);
+
+void forthread_attr_getschedpolicy(int *attr, int *policy, int *info);
+
+void forthread_attr_setschedpolicy(int *attr, int *policy, int *info);
+  
+void forthread_attr_getscope(int *attr, int *scope, int *info);
+
+void forthread_attr_setscope(int *attr, int *scope, int *info);
+
+void forthread_attr_getstacksize(int *attr, int *stacksize, int *info);
+
+void forthread_attr_setstacksize(int *attr, int *stacksize, int *info);
+  
 /*****************************************/
 /*       mutex attribute routines        */
 /*****************************************/
@@ -21,6 +68,19 @@ void forthread_attr_init(int *attr, int *info);
 void forthread_mutexattr_destroy(int *attr,int *info);
 
 void forthread_mutexattr_init(int *attr,int *info);
+
+void forthread_mutexattr_getpshared(int *attr, int *pshared, int *info);
+
+void forthread_mutexattr_setpshared(int *attr, int *pshared, int *info);
+
+void forthread_mutexattr_getprioceiling(int *attr, int *prioceiling, int *info);
+
+void forthread_mutexattr_setprioceiling(int *attr, int *prioceiling, int *info);
+
+void forthread_mutexattr_getprotocol(int *attr, int *protocol, int *info);
+void forthread_mutexattr_setprotocol(int *attr, int *protocol, int *info);
+void forthread_mutexattr_gettype(int *attr, int *type, int *info);
+void forthread_mutexattr_settype(int *attr, int *type, int *info);
 
 /*****************************************************/
 /*    condition attriubute variable routines         */
